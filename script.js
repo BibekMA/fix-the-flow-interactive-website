914f24644ef0ab20a53292a6103114669b5c9fa8
BibekMA <bibekmainali29@gmail.com>
2023-12-05 19:18:03 +0100

    Dropdown

---
+++ script.js
@@ -0,0 +1,15 @@
+function setActive(section) {
+  var offered = document.querySelector('.offered');
+  var wanted = document.querySelector('.wanted');
+
+  if (section === 'offered') {
+      offered.classList.add('active');
+      wanted.classList.remove('active');
+  } else if (section === 'wanted') {
+      wanted.classList.add('active');
+      offered.classList.remove('active');
+  }
+}
+
+
+
