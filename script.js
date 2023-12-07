442c81aa73aebd42bf0143a1aa6673ad077a0be0
BibekMA <bibekmainali29@gmail.com>
2023-12-07 20:40:53 +0100

    Update

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-function setActive(section) {
-  var offered = document.querySelector('.offered');
-  var wanted = document.querySelector('.wanted');
 
-  if (section === 'offered') {
-      offered.classList.add('active');
-      wanted.classList.remove('active');
-  } else if (section === 'wanted') {
-      wanted.classList.add('active');
-      offered.classList.remove('active');
-  }
+
+
+
+
+function setActive(section) {
+    var offered = document.querySelector('.offered');
+    var wanted = document.querySelector('.wanted');
+
+    if (section === 'offered') {
+        offered.classList.add('active');
+        wanted.classList.remove('active');
+    } else if (section === 'wanted') {
+        wanted.classList.add('active');
+        offered.classList.remove('active');
+    }
 }
 
 
